3647283e33415f97db62ece7462f71fce27563c9
YashJha22 <iyashjha@gmail.com>
2026-08-12 04:00:21 +0530

    feat: add signup step 3 profile state

--- app/src/main/java/com/skillbridge/app/screens/signup/SignupUiState.kt
+++ app/src/main/java/com/skillbridge/app/screens/signup/SignupUiState.kt
@@ -9,10 +9,20 @@ data class SignupUiState(
     val role: SignupRole? = null,
     val selectedSkills: Set<String> = emptySet(),
 
+    val bio: String ="",
+    val github: String ="",
+    val portfolio: String ="",
+
+    val companyName: String ="",
+    val hiringDescription: String ="",
+
     val fullNameError: String? = null,
     val emailError: String? = null,
     val passwordError: String? = null,
     val confirmPasswordError: String? = null,
+    val bioError: String? = null,
+    val camponyNameError: String? = null,
+    val hiringDescriptionError: String? = null,
 
     val isLoading: Boolean = false,
     val isSuccess: Boolean = false
